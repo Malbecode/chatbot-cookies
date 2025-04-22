b7bce25a62cd8375471926426c8ae69d1b9e6722
santiago-federici <santiagofederici88@gmail.com>
2025-04-22 15:37:25 -0300

    feat: install openai dependecy and create connection script

---
+++ base-ts-meta-memory/src/scripts/openai.ts
@@ -0,0 +1,24 @@
+import OpenAI from "openai";
+
+const openaiApiKey = process.env.OPENAI_API_KEY;
+const openaiModel = process.env.OPENAI_MODEL;
+
+export const chat = async (prompt, question) => {
+  try {
+    const openai = new OpenAI({ apiKey: openaiApiKey });
+    const completion = await openai.chat.completions.create({
+      model: openaiModel,
+      messages: [
+        { role: "system", content: prompt },
+        {
+          role: "user",
+          content: question,
+        },
+      ],
+    });
+    const answer = completion.choices[0].message.content;
+    return answer;
+  } catch (error) {
+    console.error("Error connecting to OpenAI API:", error);
+  }
+};
