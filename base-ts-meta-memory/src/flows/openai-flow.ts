61aeafc25af0a8389a9c628e7d5a8d724537e0e6
santiago-federici <santiagofederici88@gmail.com>
2025-04-23 08:22:43 -0300

    refactor: improve flows

--- base-ts-meta-memory/src/flows/openai-flow.ts
+++ base-ts-meta-memory/src/flows/openai-flow.ts
@@ -1,8 +1,19 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
+import { chat } from "~/scripts/openai";
+import path from "path";
+import fs from "fs";
+
+const contextPrompt = path.join(
+  process.cwd(),
+  "public/prompts",
+  "context-prompt.txt"
+);
+const fullPrompt = fs.readFileSync(contextPrompt, "utf8");
 
 export const openAIFlow = addKeyword(EVENTS.ACTION).addAction(
   async (ctx, ctxFn) => {
-    const response = "Este es el flujo de OpenAI";
+    const response = await chat(fullPrompt, ctx.body);
+
     return ctxFn.endFlow(response);
   }
 );
--- base-ts-meta-memory/src/flows/welcome-flow.ts
+++ base-ts-meta-memory/src/flows/welcome-flow.ts
@@ -1,8 +1,12 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
+import { chat } from "~/scripts/openai";
 
 export const welcomeFlow = addKeyword(EVENTS.ACTION).addAction(
   async (ctx, ctxFn) => {
-    const response = "Este es el flujo de bienvenida";
+    const response = await chat(
+      "Sos un bot encargado de saludar cordialmente a la gente. Tienes que introducirte contando que eres un asistente virtual de un emprendimiento de cookies.",
+      ctx.body
+    );
     return ctxFn.endFlow(response);
   }
 );
